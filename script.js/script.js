3388f87076c1c63b70b6d2ea56e67a9fae3bc548
Riquelme Farias <riquelmemilhomem90@gmail.com>
2024-11-19 23:29:04 -0300

    Foi adicionada a funcionalidade do Kanban

--- script.js/script.js
+++ script.js/script.js
@@ -1,23 +1,68 @@
-const cards = document.querySelectorAll("[draggable='true']")
+const cards = document.querySelectorAll(".card")
 const containerMove = document.querySelectorAll(".cards")
-
-
-function move(){
-    this.classList.add("moving")
-}
-
-function receiver(){
-    this.classList.add('columnHover')
-    const elemtMoving = document.querySelector(".moving")
-    this.appendChild(elemtMoving)
-
-}
+const add = document.querySelectorAll(".add")
+let draggedCard;
 
 cards.forEach((card)=>{
-    card.addEventListener('dragstart', move)
+    card.addEventListener('dragstart', (e)=>{
+        draggedCard = e.target
+        e.dataTransfer.effectAllowed = "move"
+    })
 })
 
-containerMove.forEach((container)=>{
-    container.addEventListener('dragover', receiver)
+containerMove.forEach((column)=>{
+    column.addEventListener('dragover', (e)=>{
+        e.preventDefault()
+    })
+
+    column.addEventListener('dragenter', (e)=>{
+        if(e.target.classList.contains("cards")){
+            e.target.classList.add("columnHover")
+        }
+    })
+
+    column.addEventListener('dragleave',(e)=>{
+        e.target.classList.remove("columnHover")
+    })
+
+    column.addEventListener('drop',(e)=>{
+        if(e.target.classList.contains("cards")){
+            e.target.classList.remove("columnHover")
+            e.target.append(draggedCard)
+        }
+    })
+
 })
 
+add.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+
+        creatCards(e)
+
+    })
+})
+
+function creatCards(e){
+    const column = e.target.parentNode.parentNode
+
+    const cardDiv = document.createElement("div")
+    cardDiv.classList.add("card")
+    cardDiv.setAttribute('draggable', 'true')
+
+    const spanName = document.createElement("span")
+    spanName.classList.add("nameCards")
+    
+    const tagsDiv = document.createElement("div")
+    tagsDiv.classList.add("tags")
+    const tags = document.createElement("span")
+    tagsDiv.appendChild(tags)
+
+    const dateDiv = document.createElement("span")
+    
+    cardDiv.appendChild(spanName)
+    cardDiv.appendChild(tagsDiv)
+    cardDiv.appendChild(dateDiv)
+
+    column.appendChild(cardDiv)
+    
+}
